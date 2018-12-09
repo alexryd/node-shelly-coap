1e112327419dda09fc6d6c709392b63abaa79b50
Alexander Ryden <alexander.ryden@gmail.com>
2018-12-09 22:11:39 +0100

    Add a method for discovering devices

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const discover = require('./lib/discover')
 const listen = require('./lib/listen')
 const Options = require('./lib/options')
 
 Options.register()
 
 module.exports = {
+  discover,
   listen,
   Options,
 }
