6342a06d88bc2fc556fce5432cf1943d77a064df
Alexander Ryden <alexander.ryden@gmail.com>
2018-12-10 09:12:11 +0100

    Create a class for managing device descriptions and status updates

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Device = require('./lib/device')
 const discover = require('./lib/discover')
 const listen = require('./lib/listen')
 const Options = require('./lib/options')
@@ -5,6 +6,7 @@ const Options = require('./lib/options')
 Options.register()
 
 module.exports = {
+  Device,
   discover,
   listen,
   Options,
